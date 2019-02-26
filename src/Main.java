77a3b04455cc1496d95a6e636253f9e988df143f
Renae Heinrich <renae.heinrich@c02wx3xejg5j.myob.myobcorp.net>
2019-02-26 11:09:50 +1100

    working but isAlive is always true

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,16 @@ class SimpleBattleShip {
     private int[] battleShip;
     private int numOfHits;
 
-    public int getNumOfHits() {
-        return numOfHits;
-    }
-
-    public void setNumOfHits(int numHits) {
-        numOfHits = numHits;
-    }
-
-    public int[] battleShip() {
-        return battleShip;
-    }
-
     public void setBattleShip(int[] locationCells) {
         battleShip = locationCells;
     }
-
 
     public String checkIfHitOrMiss(String guess) {
         //get the user guess as a String parameter
         // CONVERT the user guess to an int
         int guessToInt = Integer.parseInt(guess);
 
-        String result = "Miss";
+        String result = "Miss!";
         //REPEAT with each of the location cells in the int array
         // COMPARE the user guess to the location cell
         // IF the user guess matched
@@ -38,7 +25,7 @@ class SimpleBattleShip {
                 break;
             }
         }
-        //if number of hits equals the length of the
+        //if number of hits equals the length of the length of the battleship
         if (numOfHits == battleShip.length) {
             result = "Kill";
         }
@@ -61,7 +48,6 @@ public class Main {
         int randomNumber = min + (int) (Math.random() * max);
 
         //MAKE an int array with 3 ints using the randomly generated number, that number incremented by 1, and that number incremented by 2.
-        // random num = 3, 3+ 1 = 4, 3+ 2 = 5.
         int[] battleShip = {randomNumber, randomNumber + 1, randomNumber + 2};
 //        printArray(battleShip);
 
@@ -69,18 +55,20 @@ public class Main {
         game.setBattleShip(battleShip);
 
         System.out.println("~~ Simple Battleship ~~ \nTo guess where the BattleShip is, guess the position of where the ship is, starting from 0.");
-        while(isAlive){
+
+        while(isAlive == true){
             Scanner scanner = new Scanner(System.in);
 
             System.out.print("Please enter your guess: ");
             String userGuess = scanner.nextLine();
             String result = game.checkIfHitOrMiss(userGuess);
             numOfGuesses ++;
+
             if (result.equals("Kill")){
                 isAlive = false;
                 System.out.println("Number of guesses: " + numOfGuesses);
+                break;
             }
-
 
         }
 
